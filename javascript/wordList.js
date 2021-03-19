01ae6bf7339d1cec94b3113f06eaa701f1de0f81
vishan121 <74609997+vishan121@users.noreply.github.com>
2021-03-19 10:21:59 +0400

    added a word list system

---
+++ javascript/wordList.js
@@ -0,0 +1,22 @@
+//greetings
+var hello = [
+    "Hello my friends. How are you today?",
+    "Hey, welcome on board",
+    "Hello there, how can i help you?"
+]
+
+//open link
+const unihubLink = "https://myunihub.mdx.ac.uk/";
+var open = [
+    "here is the link"
+]
+
+//grades
+var grades = [
+    "The grading system is found in My Status. You can access grading for all your modules"
+]
+
+//calender system
+var calender = [
+
+]
