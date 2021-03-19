d5276901c0486d0af01ae48f9e5de7d064c887a2
vishan121 <74609997+vishan121@users.noreply.github.com>
2021-03-19 11:02:20 +0400

    added full word list

--- javascript/wordList.js
+++ javascript/wordList.js
@@ -1,22 +1,45 @@
 //greetings
 var hello = [
     "Hello my friends. How are you today?",
-    "Hey, welcome on board",
+    "Hey, welcome on board!",
     "Hello there, how can i help you?"
 ]
 
 //open link
 const unihubLink = "https://myunihub.mdx.ac.uk/";
 var open = [
-    "here is the link"
+    "here is the link."
 ]
 
 //grades
 var grades = [
-    "The grading system is found in My Status. You can access grading for all your modules"
+    "The grading system is found in My Status. You can access grading for all your modules."
 ]
 
 //calender system
 var calender = [
+    "The calender is found in My Learning. You view your schedule class, upcoming submission and attendance on previous class."
+]
 
+//progress
+var progress = [
+    "All information is display in My Status. You can check your last year results."
+]
+
+//timetable
+var module = [
+    "The module system can be found in My learning section. Once entered, you will find all your module on the right hand side of your screen."
+]
+
+//email
+const emailLink = "https://shorturl.at/clzR2"
+var email = [
+    "Opening email link for you to sign in."
+]
+
+//goodbye
+var goodbye = [
+    "Thank you for using this service.",
+    "Bye, have a great time",
+    "It was a pleasure to talk with you today. Hope to see you soon"
 ]
