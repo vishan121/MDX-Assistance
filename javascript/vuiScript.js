a472bb77e147c636bf7cc05d15b07bed78fe698b
vishan121 <74609997+vishan121@users.noreply.github.com>
2021-03-20 14:55:53 +0400

    add speech synthesis and read command

--- javascript/vuiScript.js
+++ javascript/vuiScript.js
@@ -1,40 +1,80 @@
-const speak = document.querySelector("#speakBtn");
-
-const recognition = new webkitSpeechRecognition();
-recognition.continuous = true;
-recognition.lang = "en-UK";
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-const synth = window.speechSynthesis;
-
-speak.addEventListener("click", () => {
-    recognition.start();
-});
-
-let utter = new SpeechSynthesisUtterance();
-utter.onend = () =>{
-    recognition.start();
+window.onload = function(){
+    play("Hello my Friend.How are you today?");
 }
 
-recognition.onresult = (e) => {
-    const transcript = e.results[e.results.length - 1][0].transcript.trim();
+const texts = document.querySelector('.texts');
 
-    if (transcript === "hello"){
-        recognition.stop();
-        utter.text = "Hello my friend. How are you today?";
-        synth.speak(utter);
-    }else if (transcript === "goodbye"){
-        recognition.stop();
-        utter.text = "It was a pleasure to talk with you today. Hope to see you soon";
-        synth.speak(utter)
-    }else if (transcript === "open"){
-        recognition.stop();
-        utter.text = "Here is the link to unihub";
-        synth.speak(utter);
-        console.log("https://unihub.mdx.ac.uk/");
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+p.classList.add("sent");
+
+recognition.addEventListener('result', (e) =>{
+    const text = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join('');
+
+    p.innerText = text;
+    document.getElementById('texts').appendChild(p);
+    var DivObj = document.getElementById("scrollBox");
+    DivObj.scrollTop = DivObj.scrollHeight;
+
+    if(e.results[0].isFinal){
+
+        var speakBtn = document.getElementById('speakBtn');
+        speakBtn.classList = "speechOn";
+
+        speechCheck(text);
+
+        var object = document.getElementById("scrollBox");
+        object.scrollTop = object.scrollHeight;
+
+        p=document.createElement('p');
+        p.classList.add("sent");
     }
+})
 
-    console.log(transcript);
-    console.log(utter.text);
-};
+function play(text){
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = 1;
+    utterance.pitch = 1;
+    speechSynthesis.speak(utterance);
+}
+function validateText(text) {
+    text = text.replace(/\s\s+/g, ' ');
+    var text = text.toLowerCase();
+    text = text.replace('?', '');
+    text = text.replace('!', '');
+    text = text.replace('`', '');
+    text = text.replace('~', '');
+    text = text.replace('@', '');
+    text = text.replace('#', '');
+    text = text.replace('$', '');
+    text = text.replace('%', '');
+    text = text.replace('^', '');
+    text = text.replace('&', '');
+    text = text.replace('*', '');
+    text = text.replace('(', '');
+    text = text.replace(')', '');
+    text = text.replace('-', '');
+    text = text.replace('_', '');
+    text = text.replace('=', '');
+    text = text.replace('+', '');
+    text = text.replace('[', '');
+    text = text.replace('{', '');
+    text = text.replace(']', '');
+    text = text.replace('}', '');
+    text = text.replace(';', '');
+    text = text.replace(':', '');
+    text = text.replace('\'', '');
+    text = text.replace('"', '');
+    text = text.replace('\\', '');
+    text = text.replace('|', '');
+    text = text.replace(',', '');
+    text = text.replace('<', '');
+    text = text.replace('.', '');
+    text = text.replace('>', '');
+    text = text.replace('/', '');
+    return text;
+}
